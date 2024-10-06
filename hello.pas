d9d730a9c1353fc693ed4f84e4422c1ac6831a3b
Яков Батуев <yaxa499@gmail.com>
2024-10-06 18:35:05 +0300

    Добали программу, выдающую приветсвие на 3 языках

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var lan := ReadInteger('Выберите цыфру соответсвующую языку: 1 - Русский, 2 - English, 3 - Français');
+  Assert(lan in 1..3);
+  case lan of
+    1: Print('Привет');
+    2: Print('Hello');
+    3: Print('Salut');
+  end;
+end.
